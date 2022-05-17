a0548d495abf80eca58eddc9ea0e1e015cc9d84c
NotFunny2 <102611579+NotFunny2@users.noreply.github.com>
2022-05-17 15:32:40 +0200

    Update best game ever.py

--- best game ever.py
+++ best game ever.py
@@ -1,21 +1,23 @@
 # poop game
 
 # ask the name and prints some stuff
-
+ans = ""
 print("Narrator: You are on some guys basement")
 name = input("Your kidnapper: Welcome to my basement whats your name?: ")
 print (f"Thats a stupid name, anyway {name}, wha- *The fbi enters the basement you where in*")
-ans = input("what do you do? kill them or kill your kidnapper?: ").lower()
 
 # the end of the game, has errors im to lazy to fix
-if ans == "kill them":
-    print("After killing the fbi you and your kidnapper start dancing until you pass out for lack of slepping")
 
-elif ans == "kill my kidnapper":
-    print("The fbi kills you cause they are bored lmao")
+while ans not in ["kill them", "kill my kidnapper"]:
+    ans = input("what do you do? kill them or kill your kidnapper? (type kill them or kill my kidnapper): ").lower()
 
-else:
-    print("You broke the game congratulations")
+
+if ans in ["kill them", "kill my kidnapper"]:
+    if ans == "kill them":
+        print("After killing the fbi you and your kidnapper start dancing until you pass out for lack of slepping")
+
+    elif ans == "kill my kidanpper":
+        print("The fbi kills you cause they are bored lmao")
 
 # the pain is over yayyyyyyyyyyy
 input("The game is over go awayyyyyyyyyyyyyyyyyyyyyyyyyyy")
